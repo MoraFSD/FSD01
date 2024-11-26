46d41390491ca900af034cd4f26a5fa144993d7d
FSD NoteBook <mirosramiros@yandex.ru>
2024-11-26 20:28:22 +0500

    new file:   .idea/.gitignore 	new file:   .idea/FSD01.iml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   Python/DBConn.py 	new file:   Python/__pycache__/DBConn.cpython-312.pyc 	new file:   Python/main.py 	modified:   index.html

---
+++ Python/main.py
@@ -0,0 +1,17 @@
+# import psycopg2
+# from DBConn import host, database, user, password
+#
+# conn = psycopg2.connect(
+#     dbname=database,
+#     user=user,
+#     password=password,
+#     host=host
+# )
+# cursor = conn.cursor()
+#
+# #Построение запроса
+# cursor.execute(
+#     "select * from userdata;"
+# )
+#
+# conn.close()
