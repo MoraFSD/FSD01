33d5a0b0826cec9f0bb1d8cf035a347787d2b6d4
FSDPC <mirosramiros@yandex.ru>
2024-11-26 20:31:16 +0500

    modified:   index.html 	modified:   solid/JavaScript/main.js

--- solid/JavaScript/main.js
+++ solid/JavaScript/main.js
@@ -1,4 +1,5 @@
-document.getElementById("open-salesbox-btn").addEventListener(saleStat)
+//document.getElementById("open-salesbox-btn").addEventListener(saleStat);
 function saleStat(){
-
+console.log("кнопка Нажата");
 }
+console.log(document.body.childNodes);
